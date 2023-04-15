4d151e4522ab24676e4213c7b07b91edec803ca9
Areeba Shakeel <shakeelareeba02@gmail.com>
2023-04-15 16:41:28 +0530

    list

---
+++ t1.py
@@ -0,0 +1,15 @@
+import turtle
+from turtle import *
+pencolor('red')
+bgcolor('yellow')
+begin_fill('blue')
+speed('slowest')
+fd(80)
+lt(120)
+fd(80)
+lt(120)
+fd(80)
+lt(129)
+end_fill()
+
+mainloop()
---
+++ t3.py
@@ -0,0 +1,12 @@
+from turtle import *
+fillcolor('red')
+begin_fill()
+for i in range(5):
+    fd(100)
+    lt(72)
+end_fill()
+fillcolor('yellow')
+begin_fill()
+circle(50)
+end_fill()
+mainloop()
